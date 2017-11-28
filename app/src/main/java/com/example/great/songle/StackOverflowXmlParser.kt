c28ec731dcd6ee4480999492e1d5753e2bbdbfb8
Battery233 <Battery233@users.noreply.github.com>
2017-11-28 21:33:36 +0000

    XMLParser changed

--- app/src/main/java/com/example/great/songle/StackOverflowXmlParser.kt
+++ app/src/main/java/com/example/great/songle/StackOverflowXmlParser.kt
@@ -1,13 +1,6 @@
 package com.example.great.songle
 
-import android.os.DropBoxManager
-import android.util.Xml
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserException
-import java.io.IOException
-import java.io.InputStream
 import java.net.URL
-import java.util.Map
 
 /**
  * Created by great on 2017/11/5.
